a428cd58e956996ed1cebdd48470fb23c69c2908
herker <her12037@byui.edu>
2015-05-16 20:56:30 -0600

    change

--- Test.java
+++ Test.java
@@ -15,7 +15,7 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello GitHub World");
+        System.out.println("Hello GitHubWorld");
         System.out.println("Testing...");
     }
     
