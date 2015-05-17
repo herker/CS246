c7e289d8dea8bc7571e2f81c7d1afc58c03439f4
herker <her12037@byui.edu>
2015-05-16 20:27:13 -0600

    correction

--- Test.java
+++ Test.java
@@ -16,7 +16,7 @@ public class Test {
      */
     public static void main(String[] args) {
         System.out.println("Hello GitHub World");
-        System.out.println("Testing...");
+//        System.out.println("Testing...");
     }
     
 }
